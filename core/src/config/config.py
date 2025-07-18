a16f3d672c9e52c76ba2f26a2b28937ef4a77d4a
Jai Aggarwal <jaiaggarwal14032002@gmail.com>
2025-07-18 20:57:48 +0530

    removing the unnecesary name

--- core/src/config/config.py
+++ core/src/config/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     MONGO_URI: str
     DB_NAME: str = "bernerspace"
     GCP_BUCKET: str
-    GOOGLE_APPLICATION_CREDENTIALS = settings.GOOGLE_APPLICATION_CREDENTIALS or "" 
+    GCP_CREDENTIALS = settings.GCP_CREDENTIALS or "" 
 
     class Config:
         env_file = ".env"

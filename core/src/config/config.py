25e51ec3bba8b42775beebd3eb02af6179bb16cf
Jai Aggarwal <jaiaggarwal14032002@gmail.com>
2025-07-19 03:15:50 +0530

    error

--- core/src/config/config.py
+++ core/src/config/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str
-    DB_NAME: str = bernerspace
+    DB_NAME: str = "bernerspace"
     GCP_BUCKET: str
     GOOGLE_APPLICATION_CREDENTIALS: str
     GITHUB_CLIENT_ID: str

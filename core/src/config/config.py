075bb1f0df2866c26a5ab0101f04bcb153b260a6
Jai Aggarwal <jaiaggarwal14032002@gmail.com>
2025-07-18 20:56:14 +0530

    changes

--- core/src/config/config.py
+++ core/src/config/config.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URI: str
     DB_NAME: str = "bernerspace"
     GCP_BUCKET: str
-    GOOGLE_APPLICATION_CREDENTIALS: str 
+    GOOGLE_APPLICATION_CREDENTIALS = settings.GOOGLE_APPLICATION_CREDENTIALS or "" 
 
     class Config:
         env_file = ".env"
